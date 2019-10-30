0cf4978a480b10e0242330a64e1d216abc5803fa
walle13 <lcz.13zxj@qq.com>
2019-10-30 15:13:22 +0800

    new demo

---
+++ Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace UART_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelout = new System.Windows.Forms.Label();
+            this.num1 = new System.Windows.Forms.Button();
+            this.num2 = new System.Windows.Forms.Button();
+            this.num3 = new System.Windows.Forms.Button();
+            this.num4 = new System.Windows.Forms.Button();
+            this.num5 = new System.Windows.Forms.Button();
+            this.num6 = new System.Windows.Forms.Button();
+            this.num7 = new System.Windows.Forms.Button();
+            this.num8 = new System.Windows.Forms.Button();
+            this.num9 = new System.Windows.Forms.Button();
+            this.num10 = new System.Windows.Forms.Button();
+            this.clean = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.multiplication = new System.Windows.Forms.Button();
+            this.division = new System.Windows.Forms.Button();
+            this.labelbefore = new System.Windows.Forms.Label();
+            this.labelmode = new System.Windows.Forms.Label();
+            this.calculate = new System.Windows.Forms.Button();
+            this.point = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelout
+            // 
+            this.labelout.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelout.Font = new System.Drawing.Font("宋体", 25.25F);
+            this.labelout.Location = new System.Drawing.Point(12, 9);
+            this.labelout.Name = "labelout";
+            this.labelout.Size = new System.Drawing.Size(482, 128);
+            this.labelout.TabIndex = 0;
+            this.labelout.Text = "0";
+            this.labelout.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // num1
+            // 
+            this.num1.Font = new System.Drawing.Font("新宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.num1.Location = new System.Drawing.Point(18, 140);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(90, 60);
+            this.num1.TabIndex = 1;
+            this.num1.Text = "1";
+            this.num1.UseVisualStyleBackColor = true;
+            this.num1.Click += new System.EventHandler(this.num1_Click);
+            // 
+            // num2
+            // 
+            this.num2.Font = new System.Drawing.Font("新宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.num2.Location = new System.Drawing.Point(114, 140);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(90, 60);
+            this.num2.TabIndex = 2;
+            this.num2.Text = "2";
+            this.num2.UseVisualStyleBackColor = true;
+            this.num2.Click += new System.EventHandler(this.num2_Click);
+            // 
+            // num3
+            // 
+            this.num3.Font = new System.Drawing.Font("新宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.num3.Location = new System.Drawing.Point(208, 141);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(90, 60);
+            this.num3.TabIndex = 3;
+            this.num3.Text = "3";
+            this.num3.UseVisualStyleBackColor = true;
+            this.num3.Click += new System.EventHandler(this.num3_Click);
+            // 
+            // num4
+            // 
+            this.num4.Font = new System.Drawing.Font("新宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.num4.Location = new System.Drawing.Point(18, 206);
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(90, 60);
+            this.num4.TabIndex = 4;
+            this.num4.Text = "4";
+            this.num4.UseVisualStyleBackColor = true;
+            this.num4.Click += new System.EventHandler(this.num4_Click);
+            // 
+            // num5
+            // 
+            this.num5.Font = new System.Drawing.Font("新宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.num5.Location = new System.Drawing.Point(113, 207);
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(90, 60);
+            this.num5.TabIndex = 5;
+            this.num5.Text = "5";
+            this.num5.UseVisualStyleBackColor = true;
+            this.num5.Click += new System.EventHandler(this.num5_Click);
+            // 
+            // num6
+            // 
+            this.num6.Font = new System.Drawing.Font("新宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.num6.Location = new System.Drawing.Point(208, 206);
+            this.num6.Name = "num6";
+            this.num6.Size = new System.Drawing.Size(90, 60);
+            this.num6.TabIndex = 6;
+            this.num6.Text = "6";
+            this.num6.UseVisualStyleBackColor = true;
+            this.num6.Click += new System.EventHandler(this.num6_Click);
+            // 
+            // num7
+            // 
+            this.num7.Font = new System.Drawing.Font("新宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.num7.Location = new System.Drawing.Point(17, 272);
+            this.num7.Name = "num7";
+            this.num7.Size = new System.Drawing.Size(90, 60);
+            this.num7.TabIndex = 7;
+            this.num7.Text = "7";
+            this.num7.UseVisualStyleBackColor = true;
+            this.num7.Click += new System.EventHandler(this.num7_Click);
+            // 
+            // num8
+            // 
+            this.num8.Font = new System.Drawing.Font("新宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.num8.Location = new System.Drawing.Point(114, 272);
+            this.num8.Name = "num8";
+            this.num8.Size = new System.Drawing.Size(90, 60);
+            this.num8.TabIndex = 8;
+            this.num8.Text = "8";
+            this.num8.UseVisualStyleBackColor = true;
+            this.num8.Click += new System.EventHandler(this.num8_Click);
+            // 
+            // num9
+            // 
+            this.num9.Font = new System.Drawing.Font("新宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.num9.Location = new System.Drawing.Point(208, 272);
+            this.num9.Name = "num9";
+            this.num9.Size = new System.Drawing.Size(90, 60);
+            this.num9.TabIndex = 9;
+            this.num9.Text = "9";
+            this.num9.UseVisualStyleBackColor = true;
+            this.num9.Click += new System.EventHandler(this.num9_Click);
+            // 
+            // num10
+            // 
+            this.num10.Font = new System.Drawing.Font("新宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.num10.Location = new System.Drawing.Point(113, 338);
+            this.num10.Name = "num10";
+            this.num10.Size = new System.Drawing.Size(90, 60);
+            this.num10.TabIndex = 10;
+            this.num10.Text = "0";
+            this.num10.UseVisualStyleBackColor = true;
+            this.num10.Click += new System.EventHandler(this.num10_Click);
+            // 
+            // clean
+            // 
+            this.clean.Font = new System.Drawing.Font("新宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.clean.Location = new System.Drawing.Point(400, 272);
+            this.clean.Name = "clean";
+            this.clean.Size = new System.Drawing.Size(90, 60);
+            this.clean.TabIndex = 11;
+            this.clean.Text = "C";
+            this.clean.UseVisualStyleBackColor = true;
+            this.clean.Click += new System.EventHandler(this.clean_Click);
+            // 
+            // plus
+            // 
+            this.plus.Font = new System.Drawing.Font("新宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.plus.Location = new System.Drawing.Point(304, 140);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(90, 60);
+            this.plus.TabIndex = 12;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // minus
+            // 
+            this.minus.Font = new System.Drawing.Font("新宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.minus.Location = new System.Drawing.Point(304, 206);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(90, 60);
+            this.minus.TabIndex = 13;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // multiplication
+            // 
+            this.multiplication.Font = new System.Drawing.Font("新宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.multiplication.Location = new System.Drawing.Point(304, 272);
+            this.multiplication.Name = "multiplication";
+            this.multiplication.Size = new System.Drawing.Size(90, 60);
+            this.multiplication.TabIndex = 14;
+            this.multiplication.Text = "*";
+            this.multiplication.UseVisualStyleBackColor = true;
+            this.multiplication.Click += new System.EventHandler(this.multiplication_Click);
+            // 
+            // division
+            // 
+            this.division.Font = new System.Drawing.Font("新宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.division.Location = new System.Drawing.Point(304, 338);
+            this.division.Name = "division";
+            this.division.Size = new System.Drawing.Size(90, 60);
+            this.division.TabIndex = 15;
+            this.division.Text = "/";
+            this.division.UseVisualStyleBackColor = true;
+            this.division.Click += new System.EventHandler(this.division_Click);
+            // 
+            // labelbefore
+            // 
+            this.labelbefore.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelbefore.Font = new System.Drawing.Font("宋体", 25.25F);
+            this.labelbefore.Location = new System.Drawing.Point(17, 9);
+            this.labelbefore.Name = "labelbefore";
+            this.labelbefore.Size = new System.Drawing.Size(477, 42);
+            this.labelbefore.TabIndex = 16;
+            this.labelbefore.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // labelmode
+            // 
+            this.labelmode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelmode.Font = new System.Drawing.Font("宋体", 25.25F);
+            this.labelmode.Location = new System.Drawing.Point(17, 51);
+            this.labelmode.Name = "labelmode";
+            this.labelmode.Size = new System.Drawing.Size(477, 42);
+            this.labelmode.TabIndex = 17;
+            this.labelmode.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // calculate
+            // 
+            this.calculate.Font = new System.Drawing.Font("新宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.calculate.Location = new System.Drawing.Point(208, 338);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(90, 60);
+            this.calculate.TabIndex = 18;
+            this.calculate.Text = "=";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // point
+            // 
+            this.point.Font = new System.Drawing.Font("新宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.point.Location = new System.Drawing.Point(17, 338);
+            this.point.Name = "point";
+            this.point.Size = new System.Drawing.Size(90, 60);
+            this.point.TabIndex = 19;
+            this.point.Text = ".";
+            this.point.UseVisualStyleBackColor = true;
+            this.point.Click += new System.EventHandler(this.point_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 415);
+            this.Controls.Add(this.point);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.labelmode);
+            this.Controls.Add(this.labelbefore);
+            this.Controls.Add(this.division);
+            this.Controls.Add(this.multiplication);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.clean);
+            this.Controls.Add(this.num10);
+            this.Controls.Add(this.num9);
+            this.Controls.Add(this.num8);
+            this.Controls.Add(this.num7);
+            this.Controls.Add(this.num6);
+            this.Controls.Add(this.num5);
+            this.Controls.Add(this.num4);
+            this.Controls.Add(this.num3);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.labelout);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "CALC";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelout;
+        private System.Windows.Forms.Button num1;
+        private System.Windows.Forms.Button num2;
+        private System.Windows.Forms.Button num3;
+        private System.Windows.Forms.Button num4;
+        private System.Windows.Forms.Button num5;
+        private System.Windows.Forms.Button num6;
+        private System.Windows.Forms.Button num7;
+        private System.Windows.Forms.Button num8;
+        private System.Windows.Forms.Button num9;
+        private System.Windows.Forms.Button num10;
+        private System.Windows.Forms.Button clean;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button multiplication;
+        private System.Windows.Forms.Button division;
+        private System.Windows.Forms.Label labelbefore;
+        private System.Windows.Forms.Label labelmode;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.Button point;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UART_test
+{
+    public partial class Form1 : Form
+    {
+        double number1 = 0, number2 = 0,result =0;
+        int inputnumber, pointnumber;
+
+        enum Operator { none, plus, minus, multiplication, division }
+        Operator mode = Operator.none;
+        bool ismode = false, isequal = false, ispoint = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void num1_Click(object sender, EventArgs e)
+        {
+            inputnumber = 1;
+            calll(inputnumber);
+        }
+
+        private void num2_Click(object sender, EventArgs e)
+        {
+            inputnumber = 2;
+            calll(inputnumber);
+        }
+
+        private void num3_Click(object sender, EventArgs e)
+        {
+            inputnumber = 3;
+            calll(inputnumber);
+        }
+
+        private void num4_Click(object sender, EventArgs e)
+        {
+            inputnumber = 4;
+            calll(inputnumber);
+        }
+
+        private void num5_Click(object sender, EventArgs e)
+        {
+            inputnumber = 5;
+            calll(inputnumber);
+        }
+
+        private void num6_Click(object sender, EventArgs e)
+        {
+            inputnumber = 6;
+            calll(inputnumber);
+        }
+
+        private void num7_Click(object sender, EventArgs e)
+        {
+            inputnumber = 7;
+            calll(inputnumber);
+        }
+
+        private void num8_Click(object sender, EventArgs e)
+        {
+            inputnumber = 8;
+            calll(inputnumber);
+        }
+
+        private void num9_Click(object sender, EventArgs e)
+        {
+            inputnumber = 9;
+            calll(inputnumber);
+        }
+
+        private void num10_Click(object sender, EventArgs e)
+        {
+            inputnumber = 0;
+            calll(inputnumber); 
+        }
+
+        private void clean_Click(object sender, EventArgs e)
+        {
+            cleanall();
+        }
+
+        public void calll(int an)
+        {
+            if (pointnumber == 0)
+            {
+                if (mode == Operator.none)
+                {
+                    number1 = number1 * 10 + an;
+                    labelout.Text = Convert.ToString(number1);
+                }
+                else
+                {
+                    number2 = number2 * 10 + an;
+                    labelout.Text = Convert.ToString(number2);
+                }
+            }
+            else
+            {
+                pointnumber = pointnumber + 1;
+                if (mode == Operator.none)
+                {
+                    number1 = number1 + an / Math.Pow(10, pointnumber - 1); //Pow =Power 乘方
+                    //number1 = number1 + an / Math.Pow(10, -1 * (pointnumber - 1));//这种方式也可以
+                    labelout.Text = Convert.ToString(number1);
+                }
+                else
+                {
+                    number2 = number2 + an / Math.Pow(10, pointnumber - 1); //Pow =Power 乘方
+                    labelout.Text = Convert.ToString(number2);
+                }
+            }
+            
+            
+            
+            //isequal = true;
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            mode = Operator.plus;
+            switchmode();
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            mode = Operator.minus;
+            switchmode();
+        }
+
+        private void multiplication_Click(object sender, EventArgs e)
+        {
+            mode = Operator.multiplication;
+            switchmode();
+        }
+
+        private void division_Click(object sender, EventArgs e)
+        {
+            mode = Operator.division;
+            switchmode();
+        }
+
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            Keys key = e.KeyCode;
+            if (e.Control != true)//如果没按Ctrl键
+                return;
+            switch (key)
+            {
+                case Keys.NumPad1:
+                    //按下小键盘0以后
+                    break;
+               
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '1')
+            {
+                num1.PerformClick();// 执行按钮“1”的操作
+            }
+            else if (e.KeyChar == '2')
+            {
+                num2.PerformClick();// 执行按钮“2”的操作
+            }
+            else if (e.KeyChar == '3')
+            {
+                num3.PerformClick();// 执行按钮“3”的操作
+            }
+            else if (e.KeyChar == '4')
+            {
+                num4.PerformClick();// 执行按钮“4”的操作
+            }
+            else if (e.KeyChar == '5')
+            {
+                num5.PerformClick();// 执行按钮“5”的操作
+            }
+            else if (e.KeyChar == '6')
+            {
+                num6.PerformClick();// 执行按钮“6”的操作
+            }
+            else if (e.KeyChar == '7')
+            {
+                num7.PerformClick();// 执行按钮“7”的操作
+            }
+            else if (e.KeyChar == '8')
+            {
+                num8.PerformClick();// 执行按钮“8”的操作
+            }
+            else if (e.KeyChar == '9')
+            {
+                num9.PerformClick();// 执行按钮“9”的操作
+            }
+            else if (e.KeyChar == '0')
+            {
+                num10.PerformClick();// 执行按钮“0”的操作
+            }
+            else if (e.KeyChar == '+')
+            {
+                plus.PerformClick();// 执行按钮“+”的操作
+            }
+            else if (e.KeyChar == '-')
+            {
+                minus.PerformClick();// 执行按钮“-”的操作
+            }
+            else if (e.KeyChar == '*')
+            {
+                multiplication.PerformClick();// 执行按钮“*”的操作
+            }
+            else if (e.KeyChar == '/')
+            {
+                division.PerformClick();// 执行按钮“/”的操作
+            }
+            else if (e.KeyChar == '=')
+            {
+                calculate.PerformClick();// 执行按钮“=”的操作
+            }
+            else if (e.KeyChar == (char)Keys.Escape)
+            {
+                clean.PerformClick();// 执行按钮“Clean”的操作
+            }
+            else if (e.KeyChar == '.')
+            {
+                point.PerformClick();// 执行按钮“Clean”的操作
+            }
+            e.Handled = true;
+        }
+
+        private void point_Click(object sender, EventArgs e)
+        {
+            if (ispoint == false)
+            {
+                pointnumber = pointnumber + 1;
+                if (mode == Operator.none)
+                {
+                    labelout.Text = Convert.ToString(number1) + ".";
+                }
+                               
+            }
+
+        }
+
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            switch (mode)
+            {
+                case Operator.plus:
+                    result = number1 + number2;
+                    break;
+                case Operator.minus:
+                    result = number1 - number2;
+                    break;
+                case Operator.multiplication:
+                    result = number1 * number2;
+                    break;
+                case Operator.division:
+                    result = number1 / number2;
+                    break;
+            }
+            isequal = true;
+            ismode = true;
+            number1 = 0;
+            number2 = 0;
+            labelbefore.Text = "";
+            labelmode.Text = "";
+            labelout.Text = Convert.ToString(result);
+            mode = Operator.none;
+        }
+
+        public void cleanall()
+        {
+            number1 = 0;
+            number2 = 0;
+            result = 0;
+            labelbefore.Text = "";
+            labelmode.Text = "";
+            labelout.Text = Convert.ToString(result);
+            isequal = false;
+            ismode = false;
+            mode = Operator.none;
+        }
+
+        public void switchmode()
+        {
+            switch (mode)
+            {
+                case Operator.plus:
+                    labelmode.Text = "+";
+                    break;
+                case Operator.minus:
+                    labelmode.Text = "-";
+                    break;
+                case Operator.multiplication:
+                    labelmode.Text = "*";
+                    break;
+                case Operator.division:
+                    labelmode.Text = "/";
+                    break;
+            }
+            if (isequal == true)
+            {
+                number1 = result;
+            }
+            ismode = true;
+            number2 = 0;
+            ispoint = false;
+            pointnumber = 0;
+            labelbefore.Text = Convert.ToString(number1);
+            labelout.Text = Convert.ToString(number2);
+        }
+    }
+}
+
+
+
+//private void btnpre_click(object sender, eventargs e)
+//{
+//    this.treeview1.focus();//此句必要，要不然都不知道操作的是什么。
+//    sendkeys.send("{up}");
+//}
+//上面是按钮去关联键盘。
